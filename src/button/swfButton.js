30a17fb8b3b12da53e49e0513e817632f41c0a00
索丘 <zengyue.yezy@alibaba-inc.com>
2015-04-21 23:10:13 +0800

    修改uploader.js的loaderDir

--- src/button/swfButton.js
+++ src/button/swfButton.js
@@ -14,7 +14,11 @@ var $ = require('jquery'),
 
 function getBaseUrl(){
   if(window.seajs){
-    return seajs.pluginSDK ? seajs.pluginSDK.util.loaderDir : seajs.data.paths['bui'];
+    if (seajs.pluginSDK && seajs.pluginSDK.util && seajs.pluginSDK.util.loaderDir) {
+      return seajs.pluginSDK.util.loaderDir;
+    } else {
+      return seajs.data.paths['bui'];
+    }
   }
   else if(window.KISSY){
     return KISSY.Config.packages['bui'].base;
